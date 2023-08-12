9653416c5afb1d9371bc8500bd0f2b9dc251276b
Rafał Biżek <49203973+RafalBizek@users.noreply.github.com>
2023-08-12 19:11:53 +0200

    added styles

--- src/components/contactlist/ContactList.js
+++ src/components/contactlist/ContactList.js
@@ -1,15 +1,19 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectFiltersContacts } from 'redux/selectors';
 import { Contact } from 'redux/contact';
+import css from './ContactList.module.css';
 
 export const ContactList = () => {
   const filterContacts = useSelector(selectFiltersContacts);
 
   return (
-    <div>
-      <ul>
+    <div className={css.contactListContainer}>
+      <ul className={css.contactList}>
         {filterContacts.map(contact => (
-          <Contact key={contact.id} contact={contact} />
+          <li key={contact.id}>
+            <Contact contact={contact} />
+          </li>
         ))}
       </ul>
     </div>
