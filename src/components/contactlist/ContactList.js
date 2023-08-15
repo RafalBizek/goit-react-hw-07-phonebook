33e95490f30351fa88d27a54677697ae6a04c1a8
Rafał Biżek <49203973+RafalBizek@users.noreply.github.com>
2023-08-15 14:40:28 +0200

    fixbug/contact list

--- src/components/contactlist/ContactList.js
+++ src/components/contactlist/ContactList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectFiltersContacts } from 'redux/selectors';
 import { Contact } from 'redux/contact';
@@ -6,16 +6,31 @@ import css from './ContactList.module.css';
 
 export const ContactList = () => {
   const filterContacts = useSelector(selectFiltersContacts);
+  const [searchText, setSearchText] = useState('');
+
+  const filteredContacts = filterContacts.filter(contact =>
+    contact.name.toLowerCase().includes(searchText.toLowerCase())
+  );
 
   return (
     <div className={css.contactListContainer}>
-      <ul className={css.contactList}>
-        {filterContacts.map(contact => (
-          <li key={contact.id}>
-            <Contact contact={contact} />
-          </li>
-        ))}
-      </ul>
+      <div className={css.searchContainer}>
+        <input
+          type="text"
+          placeholder="Wyszukaj kontakt..."
+          value={searchText}
+          onChange={e => setSearchText(e.target.value)}
+        />
+      </div>
+      <div className={css.scrollableList}>
+        <ul className={css.contactList}>
+          {filteredContacts.map(contact => (
+            <li key={contact.id}>
+              <Contact contact={contact} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
